06813eefcb5fc5f709636d364dcdf2585914e57a
chinna-venkatesh1 <83592638+chinna-venkatesh1@users.noreply.github.com>
2021-05-03 17:52:20 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>
+        <marquee>Hello welcome</marquee>
+      </h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
